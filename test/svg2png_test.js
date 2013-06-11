12d55428cc1158215b737957128c5b3b6c866151
David Bushell <david@dbushell.com>
2013-06-11 10:36:42 +0100

    add test files

---
+++ test/svg2png_test.js
@@ -0,0 +1,21 @@
+/*!
+ * grunt-svg2png
+ * https://github.com/dbushell/grunt-svg2png
+ *
+ * Copyright (c) 2013 David Bushell
+ * Licensed under The MIT License (MIT)
+ */
+
+'use strict';
+
+var grunt = require('grunt');
+
+exports.svg2png = {
+
+    test1: function(test)
+    {
+        test.equal(1, 1, "1===1");
+        test.done();
+    }
+
+};
